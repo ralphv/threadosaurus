8abb2f7200a551ce2e6694e6f826ce035c6224d9
Ralph Varjabedian <5989699+ralphv@users.noreply.github.com>
2024-09-30 13:14:59 -0400

    fix build setup

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    collectCoverage: true,
     coverageDirectory: 'coverage',
     coverageReporters: ['text', 'lcov'],
     collectCoverageFrom: ['src/**/*.ts'], // Specify the files to collect coverage from
